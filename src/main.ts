4ec5b21d4f4b9c912f011144b0603280c4fcdac4
Oruchan Asar <oruchan.asar@gmail.com>
2022-03-05 15:49:01 +0300

    Change whitelist as true

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Tour-Guide')
