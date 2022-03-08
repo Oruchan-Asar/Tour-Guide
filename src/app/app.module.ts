b1e125f1cc6410cc9c6ddeb971e891ba9486e00f
Oruchan Asar <oruchan.asar@gmail.com>
2022-03-08 16:36:39 +0300

    Upload Auth basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PackageModule } from 'src/package/package.module';
 import { TravelerModule } from 'src/traveler/traveler.module';
+import { UsersModule } from 'src/users/users.module';
 import { GuideModule } from '../guide/guide.module';
 
 @Module({
-  imports: [GuideModule, TravelerModule, PackageModule],
+  imports: [GuideModule, TravelerModule, PackageModule, UsersModule],
 })
 export class AppModule {}
