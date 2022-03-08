7bddd9bd98c2ba0221f1c34ba3eff02f9e4c943d
Oruchan Asar <oruchan.asar@gmail.com>
2022-03-08 23:49:08 +0300

    Extend auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { PackageModule } from 'src/package/package.module';
 import { TravelerModule } from 'src/traveler/traveler.module';
 import { UsersModule } from 'src/users/users.module';
 import { GuideModule } from '../guide/guide.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [GuideModule, TravelerModule, PackageModule, UsersModule],
+  imports: [GuideModule, TravelerModule, PackageModule, UsersModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
